496804b29420d2075056f72e71264b7a5f7d5bab
gloxinia106 <gloxinia106@gmail.com>
2022-10-30 12:45:52 +0900

    solve crashed hangul

--- dbconn.php
+++ dbconn.php
@@ -12,5 +12,9 @@ if (!$conn) {
     die("연결 실패: " . mysqli_connect_error());
 }
 
+mysqli_query($conn, "set session character_set_connection=utf8;");
+mysqli_query($conn, "set session character_set_results=utf8;");
+mysqli_query($conn, "set session character_set_client=utf8;");
+
 ini_set('display_errors', 'Off');
 session_start();
